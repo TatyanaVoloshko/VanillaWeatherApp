0f007a2ebba6ff9b9a5cae1113e69976b5b26373
TatyanaVoloshko <106927709+TatyanaVoloshko@users.noreply.github.com>
2022-08-22 17:40:35 +0200

    delete forecast

--- src/index.js
+++ src/index.js
@@ -15,32 +15,7 @@ function formatDate(date) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
-    let forecastElement = document.querySelector("#forecast");
 
-    let days = ["Mon", "Tue", "Wed", "Thu" ];
-   
-    let forecastHTML = '<div class="row">';
-    days.forEach(function (day) {
-        forecastHTML = 
-         forecastHTML +
-         `
-            <div class="col-2">
-              <div class="weather-forecast-date">${day}</div>
-              <img src="http://openweathermap.org/img/wn/10n@2x.png" 
-              alt=""
-              width="42" />
-              <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperature-max">23°</span>
-                <span class="weather-forecast-temperature-min">15°</span>
-              </div>
-            </div>
-        `;
-    });
-
-    forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML = forecastHTML;
-}
 
 
 let dateElement = document.querySelector("#date");
